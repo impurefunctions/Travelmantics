d019cdceb23ff85054cadce3abbe017f271e1dd6
kesegotumisang <201101433@ub.ac.bw>
2019-08-05 21:02:25 +0200

    Initial commit

---
+++ app/src/main/java/com/andela/kesegotumisang/travelmantics/InsertActivity.java
@@ -0,0 +1,79 @@
+package com.andela.kesegotumisang.travelmantics;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class InsertActivity extends AppCompatActivity {
+
+    private FirebaseDatabase mDatabase;
+    private DatabaseReference mDBReference;
+
+    private EditText txtTitle,txtPrice, txtDescription;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_insert);
+        mDatabase = FirebaseDatabase.getInstance();
+        mDBReference = mDatabase.getReference().child("traveldeals");
+
+        txtTitle = findViewById(R.id.txtTitle);
+        txtDescription = findViewById(R.id.txtDescription);
+        txtPrice = findViewById(R.id.txtPrice);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.save_menu,menu);
+
+        return true;
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        switch (item.getItemId()){
+
+            case R.id.save_menu:
+                saveDeal();
+                Toast.makeText(this,"Deal saved",Toast.LENGTH_SHORT).show();
+                clean();
+
+                default:
+                    super.onOptionsItemSelected(item);
+        }
+        return true;
+    }
+
+    private void saveDeal() {
+
+        String title = txtTitle.getText().toString();
+        String description = txtTitle.getText().toString();
+        String price = txtTitle.getText().toString();
+
+        TravelDeal deal = new TravelDeal(title,description,price,"");
+
+        mDBReference.push().setValue(deal);
+
+    }
+
+    private void clean() {
+    }
+
+
+
+}
